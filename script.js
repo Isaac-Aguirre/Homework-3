e66561f5db331de0c9a19e1798c9cccf03eee861
Isaac Aguirre <isaac42700@gmail.com>
2020-01-28 12:09:33 -0800

    updated the script and index files

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 
 window.addEventListener('load', ()=>{
   var generateBtn = document.querySelector("#generate");
- const chars = [1, 2 , 3]
+  var lower = ["a","b","c","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var nums = ["1","2","3","4","5","6","7","8","9","0"];
+  var special = ["!","@","#","$","%","&"];
   // Write password to the #password input
   function writePassword() {
     var password = generatePassword();
@@ -18,15 +21,44 @@ window.addEventListener('load', ()=>{
 }
 
   function generatePassword() {
+    password = '';
+
+    let chars = [""];
 
     let leng = prompt("How long does the password have to be?");
     let choice1 = confirm("Include lowercase letters?");
-    let choice2 = confirm("Include uppercase letters?");
-    let choice3 = confirm("Include numbers?");
-    let choice4 = confirm("Include special characters?");
-    
 
-    password = '';
+    if (choice1){
+      password += getRandom(lower);
+      for (var i = 0; i < lower.length; i++) {
+        chars.push(lower[i]);
+      }
+    }
+    let choice2 = confirm("Include uppercase letters?");
+    if (choice2) {
+      password += getRandom(upper);
+      for (var i = 0; i < upper.length; i++) {
+        chars.push(upper[i]);
+      }
+    }
+    let choice3 = confirm("Include numbers?");
+    if (choice3) {
+      password += getRandom(nums);
+      for (var i = 0; i < nums.length; i++) {
+        chars.push(nums[i]);
+      }
+    }
+
+    let choice4 = confirm("Include special characters?");
+    if (choice4) {
+      password += getRandom(special);
+      for (var i = 0; i < special.length; i++) {
+        chars.push(special[i]);
+      }
+    }
+
+
+
     for (var i = 0; i < leng; i++) {
      password += getRandom(chars);
     }
